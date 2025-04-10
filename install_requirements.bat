85cadd2c5cebf03a5ce894d4824ccd0e4fadd764
WesleyEliel <wesleymontcho@gmail.com>
2025-04-10 10:35:36 +0100

    Fix error

--- install_requirements.bat
+++ install_requirements.bat
@@ -1,8 +1,6 @@
-asgiref==3.8.1
-Django==5.2
-sqlparse==0.5.3
-typing_extensions==4.13.1
-ua-parser==1.0.1
-ua-parser-builtins==0.18.0.post1
-user-agents==2.2.0
-wfastcgi==3.0.0
+pip install asgiref==3.8.1
+pip install Django==5.2
+pip install sqlparse==0.5.3
+pip install user-agents==2.2.0
+pip install wfastcgi==3.0.0
+pip install mod_wsgi
